3835671ec8a3e4c074baaf7366e359a788478bbb
fzaman6505 <fzaman6505@gmail.com>
2019-07-25 18:31:33 -0700

    Frontend Completed

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { Post } from '../post.model';
+
+import { PostsService } from '../posts.service';
 
 
 @Component({
@@ -13,19 +14,17 @@ import { Post } from '../post.model';
 export class PostCreateComponent {
   enteredContent = '';
   enteredTitle = '';
-  @Output() postCreated = new EventEmitter<Post>();
+
+  constructor(public postsService: PostsService) {}
 
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    const post: Post  = {
-      title: form.value.title,
-      content: form.value.content
-    };
 
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 
 }
