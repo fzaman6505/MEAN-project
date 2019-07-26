2afb5a2dacf94f1ff8bbbfa91fbdc06bd459f290
fzaman6505 <fzaman6505@gmail.com>
2019-07-26 11:25:58 -0700

    updated post-list components

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,36 +2,37 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
-import { PostsService} from '../posts.service';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
-export class PostListComponent implements OnInit, OnDestroy{
- // posts = [
-   // {title: 'First Post', content: 'This is the first post\'s content'},
-   // {title: 'Second Post', content: 'This is the second post\'s content'},
-  // {title: 'Third Post', content: 'This is the third post\'s content'},
- // ];
+  constructor(public postsService: PostsService) {}
 
- posts: Post[] = [];
- private postsSub: Subscription;
+  ngOnInit() {
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
 
- constructor(public postsService: PostsService) {}
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
 
- ngOnInit(){
-   this.postsService.getPosts();
-   this.postsSub = this.postsService.getPostUpdateListener()
-   .subscribe((posts: Post[]) => {
-     this.posts = posts;
-   });
- }
-
- ngOnDestroy(){
-   this.postsSub.unsubscribe();
- }
-
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
